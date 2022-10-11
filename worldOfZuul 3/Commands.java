efe4b9302c0fb291866b8815bed098c70134909d
DustLotus <harryzhang331@gmail.com>
2022-10-11 11:09:28 +0200

    X1 gooo change

--- worldOfZuul 3/Commands.java
+++ worldOfZuul 3/Commands.java
@@ -2,7 +2,7 @@ package worldOfZuul;
 
 public enum Commands
 {
-    GO("goo"), QUIT("quit"), HELP("help"), UNKNOWN("?");
+    GO("gooo"), QUIT("quit"), HELP("help"), UNKNOWN("?");
     
     private String commandName;
     

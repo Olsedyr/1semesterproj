7aeb1a878dbaf56b14ac1b8959f2a543cc7c44cb
Oliver Holbech <56992952+Olsedyr@users.noreply.github.com>
2022-11-15 13:21:34 +0100

    Implimentere scorefil
    Laver scorefil og læser den

--- worldOfZuul 3/textUI/CommandLineClient.java
+++ worldOfZuul 3/textUI/CommandLineClient.java
@@ -7,6 +7,9 @@ package worldOfZuul.textUI;
 
 import worldOfZuul.*;
 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author ancla
@@ -21,6 +24,9 @@ public class CommandLineClient {
         game = new Game();
         parser = new Parser(game);
     }
+
+
+
 
     public void play() {
         printWelcome();
@@ -43,6 +49,18 @@ public class CommandLineClient {
                 + Commands.USE + " + [objekt]' at at interagere med objekt og '" + Commands.Inventory + "' at se hvad du har samlede op.");
         System.out.println("Skrive '" + Commands.HELP + "' hvis du har brug for hjælp.");
         System.out.println("Når du ønkser at lukke programmet skal du skrive 'afslut'.");
+
+
+        //Læser score.txt filen
+        try {
+            Scanner reader = new Scanner(game.scoreFile);
+            System.out.println("Din sidste score var: "+reader.nextLine());
+        } catch (FileNotFoundException e) {
+            System.out.println("Kan ikke finde scorefil");
+            throw new RuntimeException(e);
+        }
+
+
         System.out.println("Held og lykke! :P");
         System.out.println();
         System.out.println("Du vågner op fra din søvn, det er eftermiddagen.");

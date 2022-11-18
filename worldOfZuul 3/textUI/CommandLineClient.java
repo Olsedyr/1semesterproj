083a022d8ef314a1d88b970a095a85575fb2d939
louip22 <louip22@student.sdu.dk>
2022-11-18 08:08:12 +0100

    Added feature of subtracting points
    Added feature of subtracting points from High Score when use of ToggleItems is not climate friendly

--- worldOfZuul 3/textUI/CommandLineClient.java
+++ worldOfZuul 3/textUI/CommandLineClient.java
@@ -37,15 +37,16 @@ public class CommandLineClient {
         System.out.println();
         System.out.println("###### Velkommen til Klimaspillet! ######");
         System.out.println("Her i Klimaspillet, påvirker dine valg klimaet, så prøv dit bedste for at hjælpe klimaet!");
+        System.out.println("Du starter med 0 point, og dit mål er at få så mange point som muligt, ud fra dine klimabevidste handlinger.");
         System.out.println();
         System.out.println("Du kan skrive '" + Commands.GO + " + [udgange]' at gå til forskellige steder, '"
-                + Commands.LOOK + "' eller '" + Commands.LOOK + " + [objekt]' at kigge rundt eller på noget objekt, \n'"
-                + Commands.USE + " + [objekt]' at at interagere med objekt og '" + Commands.Inventory + "' at se hvad du har samlede op.");
+                + Commands.LOOK + "' eller '" + Commands.LOOK + " + [objekt]' at kigge rundt eller på et objekt, \n'"
+                + Commands.USE + " + [objekt]' at at interagere med et objekt og '" + Commands.Inventory + "' for at se hvad du har samlet op.");
         System.out.println("Skrive '" + Commands.HELP + "' hvis du har brug for hjælp.");
         System.out.println("Når du ønkser at lukke programmet skal du skrive 'afslut'.");
         System.out.println("Held og lykke! :P");
         System.out.println();
-        System.out.println("Du vågner op fra din søvn, det er eftermiddagen.");
+        System.out.println("Du vågner op fra din middagslur, det er eftermiddag.");
         System.out.println(game.getRoomDescription());
     }
 

a9ba346675dbddd6e647aaf86a5b2186f48c88b2
louip22 <louip22@student.sdu.dk>
2022-11-20 17:56:31 +0100

    Added different descriptions for toggle items.
    Added different descriptions for toggle items depenant on their toggle state. Modified descriptions to match. Makes Item methods getItemLongDescription and toggleStateString unnesasary, but have not written them out of the code in case they are needed elsewhere.

--- worldOfZuul 3/Game.java
+++ worldOfZuul 3/Game.java
@@ -49,17 +49,45 @@ public class Game {
         //region ------------------------------------Items------------------------------------
         ///Toggle Items: ToggleState==True means that the current state of the object is not climate friendly
         Item.ToggleItem loftlampe, radiator, vindue, computer, køkkenlampe, tv, vandhane;
-        loftlampe = new Item.ToggleItem("Du kigger på loftlampen i dit soveværelse. Du overvejer hvorvidt det er nødvendigt at det er tændt. " +
-                "\nGardinet er trukket fra så solen skinner ind i rummet og hjælper med at lyse det op.",1,true);
-        radiator = new Item.ToggleItem("Det er en radiator i din soveværelse. Du kan mærke at rummet er dejligt varmt.",5,true);
-        vindue = new Item.ToggleItem("Der er et vindue i dit soveværelse. Udenfor kan du se at bladene er faldet af træerne, " +
-                "\nog der er rim på noget af græsset under dem.",1,true);
-        computer = new Item.ToggleItem("Det er din computer. Den er stationær, har  to skærme og du fik den til din sidste fødselsdag. " +
-                "\nNår den er i brug kan du se på din elmåler at den bruger ret meget strøm.",2,true);
-        køkkenlampe = new Item.ToggleItem("Der er en lampe i dit køkken. Du overvejer hvorvidt det er nødvendigt at have den tændt.",1,true);
-        tv = new Item.ToggleItem("Der er et TV i køkkenet. Når du vasker op er det ofte rart at have noget at se på imens, " +
-                "\nmen ellers bruger du det ikke så meget.",3,true);
-        vandhane = new Item.ToggleItem("Der er et vandhane på badeværelset.",3,true);
+        loftlampe = new Item.ToggleItem("Dette burde du ikke kunne se! pinligt...",
+                "Du kigger på loftlampen i dit soveværelse. Den er tændt. Du overvejer hvorvidt det er nødvendigt at det er tændt. " +
+                        "Gardinet er trukket fra så solen skinner ind i rummet og hjælper med at lyse det op.",
+                "Du kigger på loftlampen i dit soveværelse. Den er slukket" +
+                        "Lige nu vil det nok ikke gøre den store forskel om den er tændt eller slukket, da rummet allerede er godt lyst op.",
+                1,true);
+        radiator = new Item.ToggleItem("Dette burde du ikke kunne se! pinligt...",
+                "Det er en radiator i din soveværelse. Den er tændt. Du kan mærke at rummet er ret varmt fordi den har været tændt hele dagen, " +
+                        "og solen samtidig har varmet rummet op.",
+                "Det er radiatoren i dit soveværelse. Den er slukket lige nu, men du kan mærke at rummet stadig er dejligt varmt fra solen som skinner ind.",
+                5,true);
+        vindue = new Item.ToggleItem("Dette burde du ikke kunne se! pinligt...",
+                "Der er et vindue i dit soveværelse, som står åbent. Udenfor kan du se at bladene er faldet af træerne, og der er rim på græsset under dem." +
+                        " I rummet er der dog stadig varmt fra radiatoren og solen der skinner udenfor, men du kan mærke den kulden komme ind gennem vinduet.",
+                "Der er et vindue i det soveværelse. Det er lukket. Der er en behagelig temperatur herinde, " +
+                        "og luften er stadig lidt frisk fra vinduet sidst stod åbent.",
+                1,true);
+        computer = new Item.ToggleItem("Dette burde du ikke kunne se! pinligt...",
+                "Det er din computer. Den er stationær, har to skærme og du fik den til din sidste fødselsdag. Det hele står lige nu tændt, fra da du spillede tidligere. " +
+                        "Når den er i brug kan du se på din elmåler at den bruger ret meget strøm.",
+                " Det er din stationære computer. Den er slukket, så der er ikke længere en summen at høre fra den.",
+                2,true);
+        køkkenlampe = new Item.ToggleItem("Dette burde du ikke kunne se! pinligt...",
+                "Du kigger på køkkenlampen. Den er tændt. Du overvejer hvorvidt det er nødvendigt at det er tændt. " +
+                        "Der er flere store vinduer rundt omkring i køkkenet. Gardinerne er trukket fra så solen skinner ind i rummet og hjælper med at lyse det op.",
+                "Du kigger på loftlampen i dit soveværelse. Den er slukket. " +
+                        "Lige nu vil det nok ikke gøre den store forskel om den er tændt eller slukket, da rummet allerede er godt lyst op.",
+                1,true);
+        tv = new Item.ToggleItem("Dette burde du ikke kunne se! pinligt...",
+                "Der er et TV i køkkenet. Det er lige nu tændt, og du kan høre en nyhedsvært tale i baggrunden. Når du vasker op er det " +
+                        "ofte rart at have noget at se på imens, men ellers bruger du det ikke så meget. Og lige nu ser det ikke ud til" +
+                        " at der er sket noget du ikke har hørt om tidligere.",
+                "Der er et TV i dit køkken. Det er lige nu slukket, og bruger dermed ikke unødig strøm. " +
+                        "Du har alligevel ikke lyst til at se noget på det lige nu.",
+                3,true);
+        vandhane = new Item.ToggleItem(" Dette burde du ikke kunne se! pinligt...",
+                "Der er et vandhane på badeværelset. Den står og drypper, formentligt fra da du vaskede hænder tidligere på dagen.",
+                "Der er en vandhane på badeværelset. Lige nu er den ikke i brug, og bruger dermed ikke unødig rent vand.",
+                3,true);
 
 
         ///Choice Items
@@ -184,7 +212,7 @@ public class Game {
             }
             plus_sum_score();
             currentItem.toggleState ^= true;
-            //refer to method changing itemDescription based on toggleState
+            //refer to method changing itemDescription based on toggleState?
         } else if(currentItem instanceof Item.ChoiceItem) {
             currentItem.used = true;
             removeItem();
@@ -194,23 +222,6 @@ public class Game {
             removeItem();
         }
 
-        //method changing itemDescriptiton based on toggleState
-            //if currentItem.toggleState = true
-                //if loftlampe, itemDescription = ""
-                //if radiator, itemDescription = ""
-                //if vindue, itemDescription = ""
-                //if computer, itemDescription = ""
-                //if køkkenlampe, itemDescription = ""
-                //if tv, itemDescription = ""
-                //if vandhane, itemDescription = ""
-            //else if currentItem.toggleState = false
-                //if loftlampe, itemDescription = ""
-                //if radiator, itemDescription = ""
-                //if vindue, itemDescription = ""
-                //if computer, itemDescription = ""
-                //if køkkenlampe, itemDescription = ""
-                //if tv, itemDescription = ""
-                //if vandhane, itemDescription = ""
     }
     private void removeItem() {
         currentRoom.removeItem(currentItem);
